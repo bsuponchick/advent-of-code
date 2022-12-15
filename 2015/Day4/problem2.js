8e76c1bd9e1e7922958738bc2d05422fd5c9a9a3
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2022-12-14 21:34:55 -0600

    Adding Day 4 solutions

---
+++ 2015/Day4/problem2.js
@@ -0,0 +1,18 @@
+const md5 = require('blueimp-md5');
+
+const input = 'iwrupvqb';
+let found = false;
+let number = 0;
+
+while (!found) {
+    let test = `${input}${number}`;
+    const hash = md5(test);
+
+    if (hash.substring(0, 6) === '000000') {
+        found = true;
+    } else {
+        number++;
+    }
+}
+
+console.log(`Your secret key is ${input}${number}`);
