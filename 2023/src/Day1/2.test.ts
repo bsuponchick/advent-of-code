b706673e31f3f5dfcd1290e4107be310317932e3
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2023-12-01 17:17:21 -0600

    Day 1 Problem 2

---
+++ 2023/src/Day1/2.test.ts
@@ -0,0 +1,70 @@
+import { describe, expect, test } from '@jest/globals';
+import { getDigitsFromString } from './2';
+
+describe('Day 1 - Part 2', () => {
+    describe(`getDigitsFromString`, () => {
+        test(`'1' should return 11`, () => {
+            expect(getDigitsFromString('1')).toBe(11);
+        });
+
+        test(`'oneone' should return 11`, () => {
+            expect(getDigitsFromString('oneone')).toBe(11);
+        });
+
+        test(`'1one' should return 11`, () => {
+            expect(getDigitsFromString('1one')).toBe(11);
+        });
+
+        test(`'two1nine' should return 29`, () => {
+            expect(getDigitsFromString('two1nine')).toBe(29);
+        });
+        
+        test(`'eightwothree' should return 83`, () => {
+            expect(getDigitsFromString('eightwothree')).toBe(83);
+        });
+
+        test(`'abcone2threexyz' should return 13`, () => {
+            expect(getDigitsFromString('abcone2threexyz')).toBe(13);
+        });
+
+        test(`'xtwone3four' should return 24`, () => {
+            expect(getDigitsFromString('xtwone3four')).toBe(24);
+        });
+
+        test(`'4nineeightseven2' should return 42`, () => {
+            expect(getDigitsFromString('4nineeightseven2')).toBe(42);
+        });
+
+        test(`'zoneight234' should return 14`, () => {
+            expect(getDigitsFromString('zoneight234')).toBe(14);
+        });
+
+        test(`'7pqrstsixteen' should return 76`, () => {
+            expect(getDigitsFromString('7pqrstsixteen')).toBe(76);
+        });
+
+        test(`'fivetczxxvjrrqfive1sevennvj6one3' should return 53`, () => {
+            expect(getDigitsFromString('fivetczxxvjrrqfive1sevennvj6one3')).toBe(53);
+        });
+
+        test(`'bmjhkkn4pgf' should return 44`, () => {
+            expect(getDigitsFromString('bmjhkkn4pgf')).toBe(44);
+        });
+
+        test(`'tfrrjmcvtbmktnxtxkkrcctmc33four5gfqpcjreight' should return 38`, () => {
+            expect(getDigitsFromString('tfrrjmcvtbmktnxtxkkrcctmc33four5gfqpcjreight')).toBe(38);
+        });
+
+        test(`'6sixzvdsprdqlftwonine' should return 69`, () => {
+            expect(getDigitsFromString('6sixzvdsprdqlftwonine')).toBe(69);
+        });
+
+        test(`'6qmgkbkmlxfourprhxrxrdseight' should return 68`, () => {
+            expect(getDigitsFromString('6qmgkbkmlxfourprhxrxrdseight')).toBe(68);
+        });
+
+        test(`'248twofbkfpxtheightwovng' should return 22`, () => {
+            expect(getDigitsFromString('248twofbkfpxtheightwovng')).toBe(22);
+        });
+    });
+});
--- 2023/src/Day1/2.ts
+++ 2023/src/Day1/2.ts
@@ -2,34 +2,74 @@ const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-
-const digitsOfInput: number[][] = [];
 let calibrationValues: number[] = [];
+let digitsAsStrings: string[] = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+export const getDigitsFromString = (string: string): number => {
+    // Include line parsing logic here
+    const characters = string.split('');
+    const digits: number[] = [];
+    let digitAsString: string = '';
+
+    characters.forEach((character) => {
+        const digit = parseInt(character, 10);
+        let exactMatch = false;
+
+        if (!isNaN(digit)) {
+            digits.push(digit);
+            digitAsString = '';
+        } else {
+            digitAsString += character;
+            let valid = false;
+
+            if (debug) {
+                console.log(`Digit as string: ${digitAsString}`);
+            }
+
+            digitsAsStrings.forEach((validString) => {
+                if (validString === digitAsString) {
+                    digits.push(digitsAsStrings.indexOf(validString) + 1);
+                    exactMatch = true;
+                } else if (validString.indexOf(digitAsString) === 0) {
+                    valid = true;
+                }
+            });
+
+            if (valid === false) {
+                if (exactMatch) {
+                    digitAsString = `${character}`;
+                } else {
+                    digitAsString = digitAsString.slice(1);
+                }
+            }
+        }
+    });
+    
+    let twoDigitString = '';
+
+    if (digits) {
+        let firstDigit = digits.shift();
+        let lastDigit = digits.pop();
+
+        if (firstDigit === undefined) {
+            firstDigit = 0;
+        }
+
+        if (lastDigit === undefined) {
+            lastDigit = firstDigit;
+        }
+
+        twoDigitString = `${firstDigit}${lastDigit}`;
+    }
+
+    return Number.parseInt(twoDigitString, 10);
+}
 
 
 const execute = () => {
-    // This is where you add your code
-    digitsOfInput.forEach((digits) => {
-        let twoDigitString = '';
-
-        if (digits) {
-            let firstDigit = digits.shift();
-            let lastDigit = digits.pop();
-
-            if (firstDigit === undefined) {
-                firstDigit = 0;
-            }
-
-            if (lastDigit === undefined) {
-                lastDigit = firstDigit;
-            }
-
-            twoDigitString = `${firstDigit}${lastDigit}`;
-            
-        }
-
-        calibrationValues.push(Number.parseInt(twoDigitString, 10));
-    });
+    if (debug) {
+        console.log(`Calibration values: ${calibrationValues}`);
+    }
 
     const sum = calibrationValues.reduce((sum, value) => sum + value, 0);
     console.log(`The sum of all calibration values is ${sum}`);
@@ -37,23 +77,16 @@ const execute = () => {
 
 const parseLine = (line) => {
     // Include line parsing logic here
-    const characters = line.split('');
-    const digits: number[] = [];
+    const value = getDigitsFromString(line);
 
-    characters.forEach((character) => {
-        const digit = parseInt(character, 10);
-        if (!isNaN(digit)) {
-            digits.push(digit);
-        }
-    });
-
-    digitsOfInput.push(digits);
-    console.log(`Digits: ${digits}`);
+    console.log(`Generated ${value} from ${line}`);
+    calibrationValues.push(value);
 };
 
 var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(test ? './test.txt' : './input.txt')
+    input: require('fs').createReadStream(test ? './test2.txt' : './input.txt')
 });
+
 
 lineReader.on('line', (line) => {
     parseLine(line);
