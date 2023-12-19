9bc40c01c770ffa0bc7a9433885d092e77323c05
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2023-12-19 08:04:14 -0600

    Day 18 Problem 1

--- 2023/src/Day18/1.logic.ts
+++ 2023/src/Day18/1.logic.ts
@@ -2,9 +2,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 enum BlockType {
     Trench = 'trench',
-    Hole = 'hole',
-    Sky = 'sky',
-    Unknown = 'unknown'
+    Ground = 'ground',
+    Unknown = 'unknown',
 }
 
 export class Block {
@@ -12,10 +11,6 @@ export class Block {
     x: number = 0;
     y: number = 0;
     baseColor: string = '';
-    northColor: string = '';
-    eastColor: string = '';
-    southColor: string = '';
-    westColor: string = '';
     north: Block | null = null;
     east: Block | null = null;
     south: Block | null = null;
@@ -26,90 +21,13 @@ export class Block {
         this.id = uuidv4();
     }
 
-    paint() {
-        if (this.north) {
-            // Set the north color to nil to indicate it has no wall
-            this.northColor = 'nil';
+    setType = (type: BlockType) => {
+        this.type = type;
+    };
 
-            if (this.south) {
-                // This is a straight block, so need to set the north and south colors to nil
-                this.southColor = 'nil';
-            } else {
-                // This is a bend, determine which way it bends
-                if (this.east) {
-                    // East block exists, so south color is the east color
-                    this.eastColor = 'nil';
-                    this.southColor = this.east.baseColor;
-                } else if (this.west) {
-                    // West block exists, so south color is the west color
-                    this.westColor = 'nil';
-                    this.southColor = this.west.baseColor;
-                }
-            }
-        } else {
-            if (this.south) {
-                // This is a bend, determine which way it bends
-                if (this.east) {
-                    // East block exists, so north color is the east color
-                    this.eastColor = 'nil';
-                    this.northColor = this.east.baseColor;
-                } else if (this.west) {
-                    // West block exists, so north color is the west color
-                    this.westColor = 'nil';
-                    this.northColor = this.west.baseColor;
-                }
-            } else {
-                // This should be a block with an east and west, set those sides to 'nil'
-                this.eastColor = 'nil';
-                this.westColor = 'nil';
-            }            
-        }
-
-        // if (this.south) {
-        //     // Set the south color to nil to indicate it has no wall
-        //     this.southColor = 'nil';
-        // } else {
-        //     // No south block, so need to set the south color based on the bend
-        //     if (this.east) {
-        //         // East block exists, so south color is the east color
-        //         this.southColor = this.eastColor;
-        //     } else if (this.west) {
-        //         // West block exists, so south color is the west color
-        //         this.southColor = this.westColor;
-        //     } else {
-
-        //     }
-        // }
-
-        // if (this.east) {
-        //     // Set the east color to nil to indicate it has no wall
-        //     this.eastColor = 'nil';
-        // } else {
-        //     // No east block, so need to set the east color based on the bend
-        //     if (this.north) {
-        //         // North block exists, so east color is the north color
-        //         this.eastColor = this.northColor;
-        //     } else if (this.south) {
-        //         // South block exists, so east color is the south color
-        //         this.eastColor = this.southColor;
-        //     }
-        
-        // }
-
-        // if (this.west) {
-        //     // Set the west color to nil to indicate it has no wall
-        //     this.westColor = 'nil';
-        // } else {
-        //     // No west block, so need to set the west color based on the bend
-        //     if (this.north) {
-        //         // North block exists, so west color is the north color
-        //         this.westColor = this.northColor;
-        //     } else if (this.south) {
-        //         // South block exists, so west color is the south color
-        //         this.westColor = this.southColor;
-        //     }
-        // }
-    }
+    setBaseColor = (color: string) => {
+        this.baseColor = color;
+    };
 }
 
 export class Map {
@@ -119,17 +37,18 @@ export class Map {
         this.blocks = [];
     }
 
-    initialize = (dimensions: {width: number, height: number}) => {
-        for (let x = 0; x < dimensions.width; x++) {
+    initialize = (dimensions: { width: number; height: number }) => {
+        // Totally cheating by adding 200 to each dimension to make sure we have enough space
+        for (let x = 0; x < dimensions.height + 200; x++) {
             this.blocks[x] = [];
 
-            for (let y = 0; y < dimensions.height; y++) {
+            for (let y = 0; y < dimensions.width + 200; y++) {
                 this.blocks[x][y] = new Block();
             }
         }
 
         this.establishConnections();
-    }
+    };
 
     establishConnections = () => {
         this.blocks.forEach((row, rowIndex) => {
@@ -138,42 +57,170 @@ export class Map {
                     const neighbor = this.blocks[rowIndex - 1][columnIndex];
                     block.north = neighbor;
                 }
-    
+
                 if (columnIndex > 0) {
                     const neighbor = row[columnIndex - 1];
                     block.west = neighbor;
                 }
-    
+
                 if (rowIndex < this.blocks.length - 1) {
                     const neighbor = this.blocks[rowIndex + 1][columnIndex];
                     block.south = neighbor;
                 }
-    
+
                 if (columnIndex < row.length - 1) {
                     const neighbor = row[columnIndex + 1];
                     block.east = neighbor;
                 }
             });
         });
-    }
+    };
+
+    generateGround = (): Block => {
+        const block = new Block();
+        block.setType(BlockType.Ground);
+
+        return block;
+    };
 
     expand = () => {
         this.blocks.forEach((row) => {
-            row.unshift(new Block());
-            row.push(new Block());
+            row.unshift(this.generateGround());
+            row.push(this.generateGround());
         });
 
         const newFirstRow: Block[] = [];
         const newLastRow: Block[] = [];
 
         this.blocks[0].forEach(() => {
-            newFirstRow.push(new Block());
-            newLastRow.push(new Block());
+            newFirstRow.push(this.generateGround());
+            newLastRow.push(this.generateGround());
         });
 
         this.blocks.unshift(newFirstRow);
         this.blocks.push(newLastRow);
+    };
+
+    dig = (coordinate: { x: number; y: number }, color: string) => {
+        const block = this.blocks[coordinate.y][coordinate.x];
+        block.setBaseColor(color);
+        block.setType(BlockType.Trench);
+    };
+
+    digTrench = (startingCoordinate: { x: number; y: number }, instructions: string[]) => {
+        let currentCoordinate = startingCoordinate;
+
+        instructions.forEach((instruction, index) => {
+            const { direction, distance, color } = parseInstruction(instruction);
+            console.log(`Digging ${distance} blocks ${direction} with color ${color} from ${JSON.stringify(currentCoordinate)}}`);
+
+            if (index === 0) {
+                this.dig(currentCoordinate, color);
+            }
+
+            for (let i = 0; i < distance; i++) {
+                switch (direction) {
+                    case 'U':
+                        currentCoordinate.y--;
+                        break;
+                    case 'R':
+                        currentCoordinate.x++;
+                        break;
+                    case 'D':
+                        currentCoordinate.y++;
+                        break;
+                    case 'L':
+                        currentCoordinate.x--;
+                        break;
+                }
+
+                this.dig(currentCoordinate, color);
+            }
+        });
+    };
+    
+    identifyGroundBlocks = () => {
+        // Add an outer layer of blocks to start as ground
+        this.expand();
+        this.establishConnections();
+
+        let found = 0;
+        let started = false;
+        
+        while (found > 0 || started === false) {
+            found = 0;
+            started = true;
+
+            // Loop through all blocks and identify the ground blocks
+            for (let row = 0; row < this.blocks.length; row++) {
+                for (let column = 0; column < this.blocks[row].length; column++) {
+                    const block = this.blocks[row][column];
+
+                    if (block.type === BlockType.Unknown) {
+                        // This is an unknown block, so check if it is ground
+                        if ((block.north?.type === BlockType.Ground) || (block.east?.type === BlockType.Ground) || (block.south?.type === BlockType.Ground) || (block.west?.type === BlockType.Ground)) {
+                            // This is a ground block
+                            block.setType(BlockType.Ground);
+                            found++;
+                        }
+                    }
+                }
+            }
+        }
+
+        
+    };
+
+    digInnerTrenches = () => {
+        // Loop through all blocks and identify the ground blocks
+        for (let row = 0; row < this.blocks.length; row++) {
+            for (let column = 0; column < this.blocks[row].length; column++) {
+                const block = this.blocks[row][column];
+
+                if (block.type === BlockType.Unknown) {
+                    // This is an inner block, dig it without a paint color
+                    this.dig({ x: column, y: row }, 'nil');
+                }
+            }
+        }
     }
+
+    calculateCubitMetersOfTrench = () => {
+        let total = 0;
+
+        this.blocks.forEach((row) => {
+            row.forEach((block) => {
+                if (block.type === BlockType.Trench) {
+                    total++;
+                }
+            });
+        });
+
+        return total;
+    }
+
+    print = () => {
+        let line = '';
+        this.blocks.forEach((row) => {
+            row.forEach((block) => {
+                switch (block.type) {
+                    case BlockType.Trench:
+                        line += '#';
+                        break;
+                    case BlockType.Ground:
+                        line += ' ';
+                        break;
+                    case BlockType.Unknown:
+                        line += '?';
+                        break;
+                }
+            });
+
+            line += '\n';
+        });
+
+        console.log(`${line}`);
+    };
 }
 
 export const parseInstruction = (instruction: string) => {
@@ -182,11 +229,21 @@ export const parseInstruction = (instruction: string) => {
     return {
         direction,
         distance: parseInt(distance, 10),
-        color: color.replace('(', '').replace(')', '')
+        color: color.replace('(', '').replace(')', ''),
     };
 };
 
-export const determineBoundingBox = (instructions: string[]): { height: number; width: number, minX: number, minY: number, maxX: number, maxY: number, startingPoint: { x: number; y: number} } => {
+export const determineBoundingBox = (
+    instructions: string[],
+): {
+    height: number;
+    width: number;
+    minX: number;
+    minY: number;
+    maxX: number;
+    maxY: number;
+    startingPoint: { x: number; y: number };
+} => {
     let minX = 0;
     let maxX = 0;
     let minY = 0;
@@ -234,14 +291,14 @@ export const determineBoundingBox = (instructions: string[]): { height: number; 
     let width = Math.abs(minX) + Math.abs(maxX) + 1;
 
     let startingX = 0;
-    
-    if ((minX !== 0) && (maxX !== 0)) {
-        startingX = minX  + width - 1;
+
+    if (minX !== 0 && maxX !== 0) {
+        startingX = minX + width - 1;
     }
-    
+
     let startingY = 0;
-    
-    if ((minY !== 0) && (maxY !== 0)) {
+
+    if (minY !== 0 && maxY !== 0) {
         startingY = minY + height - 1;
     }
 
@@ -254,7 +311,7 @@ export const determineBoundingBox = (instructions: string[]): { height: number; 
         maxY,
         startingPoint: {
             x: startingX,
-            y: startingY
-        }
+            y: startingY,
+        },
     };
 };
--- 2023/src/Day18/1.ts
+++ 2023/src/Day18/1.ts
@@ -1,17 +1,29 @@
-// import {  } from "./1.logic";
+import { Map, determineBoundingBox } from "./1.logic";
 
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-
+const instructions: string[] = [];
+const map = new Map();
 
 const execute = () => {
-    
+    const boundingBox = determineBoundingBox(instructions);
+    console.log(`Bounding box: ${JSON.stringify(boundingBox)}`);
+    map.initialize(boundingBox);
+
+    map.digTrench(boundingBox.startingPoint, instructions);
+    map.identifyGroundBlocks();
+    map.digInnerTrenches();
+    map.print();
+
+    const cubicMetersOfTrench = map.calculateCubitMetersOfTrench();
+
+    console.log(`Cubic meters of trench: ${cubicMetersOfTrench}`);
 };
 
 const parseLine = (line: string) => {
-    
+    instructions.push(line);
 };
 
 var lineReader = require('readline').createInterface({
