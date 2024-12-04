3dd0d94d370e276f25eb0dd5d2b8f83d72699ee0
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2024-12-03 22:28:10 -0800

    Day 4 Problem 2

--- 2024/src/Day4/4.2.ts
+++ 2024/src/Day4/4.2.ts
@@ -3,13 +3,75 @@ const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
+const wordsearch: string[][] = [];
+const filledInWordSearch: string[][] = [];
 
 const execute = () => {
-    
+    let countOfXMAS = 0;
+
+    for (let i = 0; i < wordsearch.length; i++) {
+        for (let j = 0; j < wordsearch[i].length; j++) {
+            if (wordsearch[i][j] === 'A') {
+                if (j < wordsearch[i].length - 1 && j > 0 && i < wordsearch.length - 1 && i > 0) {
+                    // Check upper left to lower right
+                    if (wordsearch[i - 1][j - 1] === 'M' && wordsearch[i + 1][j + 1] === 'S') {
+                        // Check lower left to upper right and lower right to upper left
+                        if (wordsearch[i + 1][j - 1] === 'M' && wordsearch[i - 1][j + 1] === 'S') {
+                            countOfXMAS++;
+                            filledInWordSearch[i][j] = 'A';
+                            filledInWordSearch[i - 1][j - 1] = 'M';
+                            filledInWordSearch[i + 1][j + 1] = 'S';
+                            filledInWordSearch[i + 1][j - 1] = 'M';
+                            filledInWordSearch[i - 1][j + 1] = 'S';
+                        } else if (wordsearch[i + 1][j - 1] === 'S' && wordsearch[i - 1][j + 1] === 'M') {
+                            countOfXMAS++;
+                            filledInWordSearch[i][j] = 'A';
+                            filledInWordSearch[i - 1][j - 1] = 'M';
+                            filledInWordSearch[i + 1][j + 1] = 'S';
+                            filledInWordSearch[i + 1][j - 1] = 'S';
+                            filledInWordSearch[i - 1][j + 1] = 'M';
+                        }
+                    } else if (wordsearch[i - 1][j - 1] === 'S' && wordsearch[i + 1][j + 1] === 'M') {
+                        // Check lower left to upper right and lower right to upper left
+                        if (wordsearch[i + 1][j - 1] === 'M' && wordsearch[i - 1][j + 1] === 'S') {
+                            countOfXMAS++;
+                            filledInWordSearch[i][j] = 'A';
+                            filledInWordSearch[i - 1][j - 1] = 'S';
+                            filledInWordSearch[i + 1][j + 1] = 'M';
+                            filledInWordSearch[i + 1][j - 1] = 'M';
+                            filledInWordSearch[i - 1][j + 1] = 'S';
+                        } else if (wordsearch[i + 1][j - 1] === 'S' && wordsearch[i - 1][j + 1] === 'M') {
+                            countOfXMAS++;
+                            filledInWordSearch[i][j] = 'A';
+                            filledInWordSearch[i - 1][j - 1] = 'S';
+                            filledInWordSearch[i + 1][j + 1] = 'M';
+                            filledInWordSearch[i + 1][j - 1] = 'S';
+                            filledInWordSearch[i - 1][j + 1] = 'M';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    console.log(`The cross-MAS appears ${countOfXMAS} times.`);
+
+    if (debug) {
+        filledInWordSearch.forEach((line) => {
+            console.log(JSON.stringify(line));
+        });
+    }
 }
 
 const parseLine = (line: string) => {
-    
+    const parts = line.split('');
+    wordsearch.push(parts);
+
+    const filledInPartsArray = [];
+    parts.forEach((part) => {
+        filledInPartsArray.push('.');
+    });
+    filledInWordSearch.push(filledInPartsArray);
 };
 
 var lineReader = require('readline').createInterface({
