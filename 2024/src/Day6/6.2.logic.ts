43f611a6a6136dead17304099b40c2baf6eae2ae
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2024-12-05 22:55:00 -0800

    Working through Day 6 Part 2...close but still in an infinite loop without detecting it somehow.

---
+++ 2024/src/Day6/6.2.logic.ts
@@ -0,0 +1,202 @@
+export class Guard {
+    x: number;
+    y: number;
+    direction: string;
+    map: string[][];
+
+    constructor(map: string[][]) {
+        this.map = map;
+
+        map.forEach((row, y) => {
+            row.forEach((tile, x) => {
+                if (tile === '^' || tile === 'v' || tile === '<' || tile === '>') {
+                    this.x = x;
+                    this.y = y;
+                    this.direction = tile;
+                }
+            });
+        });
+    }
+
+    move(): boolean {
+        let nextTile: string;
+
+        switch (this.direction) {
+            case '^':
+                if (this.y === 0) {
+                    this.map[this.y][this.x] = 'X';
+                    return false;
+                }
+
+                nextTile = this.map[this.y - 1][this.x];
+                if (nextTile === '#') {
+                    this.direction = '>';
+                } else {
+                    this.map[this.y][this.x] = 'X';
+                    this.y--;
+                    this.map[this.y][this.x] = '^';
+                }
+                
+                return true;
+            case 'v':
+                if (this.y === this.map.length - 1) {
+                    this.map[this.y][this.x] = 'X';
+                    return false;
+                }
+
+                nextTile = this.map[this.y + 1][this.x];
+                if (nextTile === '#') {
+                    this.direction = '<';
+                } else {
+                    this.map[this.y][this.x] = 'X';
+                    this.y++;
+                    this.map[this.y][this.x] = 'v';
+                }
+
+                return true;
+            case '<':
+                if (this.x === 0) {
+                    this.map[this.y][this.x] = 'X';
+                    return false;
+                }
+
+                nextTile = this.map[this.y][this.x - 1];
+                if (nextTile === '#') {
+                    this.direction = '^';
+                } else {
+                    this.map[this.y][this.x] = 'X';
+                    this.x--;
+                    this.map[this.y][this.x] = '<';
+                }
+                
+                return true;
+            case '>':
+                if (this.x === this.map[0].length - 1) {
+                    this.map[this.y][this.x] = 'X';
+                    return false;
+                }
+
+                nextTile = this.map[this.y][this.x + 1];
+                if (nextTile === '#') {
+                    this.direction = 'v';
+                } else {
+                    this.map[this.y][this.x] = 'X';
+                    this.x++;
+                    this.map[this.y][this.x] = '>';
+                }
+
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    moveTwo(): boolean {
+        let nextTile: string;
+
+        switch (this.direction) {
+            case '^':
+                if (this.y === 0) {
+                    if (this.map[this.y][this.x] === '.') {
+                        this.map[this.y][this.x] = '^';
+                    } else {
+                        this.map[this.y][this.x] += '^';
+                    }
+                    return false;
+                }
+
+                nextTile = this.map[this.y - 1][this.x];
+                if (nextTile === '#' || nextTile === 'O') {
+                    this.direction = '>';
+                } else if (nextTile.indexOf('^') !== -1) {
+                    throw new Error('The guard is moving to a tile that has already been visited.');
+                } else {
+                    if (this.map[this.y][this.x] === '.') {
+                        this.map[this.y][this.x] = '^';
+                    } else {
+                        this.map[this.y][this.x] += '^';
+                    }
+                    this.y--;
+                }
+                
+                return true;
+            case 'v':
+                if (this.y === this.map.length - 1) {
+                    if (this.map[this.y][this.x] === '.') {
+                        this.map[this.y][this.x] = 'v';
+                    } else {
+                        this.map[this.y][this.x] += 'v';
+                    }
+                    return false;
+                }
+
+                nextTile = this.map[this.y + 1][this.x];
+                if (nextTile === '#' || nextTile === 'O') {
+                    this.direction = '<';
+                } else if (nextTile.indexOf('v') !== -1) {
+                    throw new Error('The guard is moving to a tile that has already been visited.');
+                } else {
+                    if (this.map[this.y][this.x] === '.') {
+                        this.map[this.y][this.x] = 'v';
+                    } else {
+                        this.map[this.y][this.x] += 'v';
+                    }
+                    this.y++;
+                }
+
+                return true;
+            case '<':
+                if (this.x === 0) {
+                    if (this.map[this.y][this.x] === '.') {
+                        this.map[this.y][this.x] = '<';
+                    } else {
+                        this.map[this.y][this.x] += '<';
+                    }
+                    return false;
+                }
+
+                nextTile = this.map[this.y][this.x - 1];
+                if (nextTile === '#' || nextTile === 'O') {
+                    this.direction = '^';
+                } else if (nextTile.indexOf('<') !== -1) {
+                    throw new Error('The guard is moving to a tile that has already been visited.');
+                } else {
+                    if (this.map[this.y][this.x] === '.') {
+                        this.map[this.y][this.x] = '<';
+                    } else {
+                        this.map[this.y][this.x] += '<';
+                    }
+                    this.x--;
+                }
+                
+                return true;
+            case '>':
+                if (this.x === this.map[0].length - 1) {
+                    if (this.map[this.y][this.x] === '.') {
+                        this.map[this.y][this.x] = '>';
+                    } else {
+                        this.map[this.y][this.x] += '>';
+                    }
+                    return false;
+                }
+
+                nextTile = this.map[this.y][this.x + 1];
+                if (nextTile === '#' || nextTile === 'O') {
+                    this.direction = 'v';
+                } else if (nextTile.indexOf('>') !== -1) {
+                    throw new Error('The guard is moving to a tile that has already been visited.');
+                } else {
+                    if (this.map[this.y][this.x] === '.') {
+                        this.map[this.y][this.x] = '>';
+                    } else {
+                        this.map[this.y][this.x] += '>';
+                    }
+                    this.x++;
+                }
+
+                return true;
+            default:
+                return false;
+        }
+    }
+}
--- 2024/src/Day6/6.2.ts
+++ 2024/src/Day6/6.2.ts
@@ -1,25 +1,83 @@
-
+import { Guard } from './6.2.logic';
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
+const map: string[][] = [];
+interface Coordinate {
+    x: number;
+    y: number;
+}
+const coordinatesOfVisitedTiles: Coordinate[] = [];
 
 const execute = () => {
-    
-}
+    const copyOfMap = map.map((row) => [...row]);
+    const guard = new Guard(copyOfMap);
+    const startingX = guard.x;
+    const startingY = guard.y;
+
+    while (guard.move()) {
+        // Do nothing.
+    }
+
+    guard.map.forEach((row, rowIndex) => {
+        row.forEach((tile, tileIndex) => {
+            if (tile === 'X') {
+                coordinatesOfVisitedTiles.push({ x: rowIndex, y: tileIndex });
+            }
+        });
+    });
+
+    console.log(`The guard visited ${coordinatesOfVisitedTiles.length} tiles.`);
+    console.log(guard.map.map((row) => row.join('')).join('\n'));
+    console.log();
+
+    let countOfPotentialObstructions = 0;
+
+    coordinatesOfVisitedTiles.forEach((coordinate, coordIndex) => {
+        console.log(`Checking obstruction ${coordIndex + 1} of ${coordinatesOfVisitedTiles.length}...`);
+        console.log(`Checking obstruction at ${coordinate.x},${coordinate.y}...`);
+        console.log(`There are ${countOfPotentialObstructions} potential obstructions so far...`);
+
+        const x = coordinate.x;
+        const y = coordinate.y;
+
+        const temporaryMap = map.map((row) => [...row]);
+        temporaryMap[x][y] = 'O';
+
+        const guard = new Guard(temporaryMap);
+
+        try {
+            while (guard.moveTwo()) {
+                // Do nothing.
+            }
+        } catch (e) {
+            if (debug) {
+                console.log(`Loop detected...`);
+                console.log(guard.map.map((row) => row.join('')).join('\n'));
+                console.log();
+            }
+            countOfPotentialObstructions++;
+        }
+    });
+
+    console.log(`There are ${countOfPotentialObstructions} potential obstructions.`);
+};
 
 const parseLine = (line: string) => {
-    
+    map.push(line.split(''));
 };
 
 var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream(test ? './test.txt' : './input.txt')
+    input: require('fs').createReadStream(test ? './test.txt' : './input.txt'),
 });
 
-lineReader.on('line', (line) => {
-    parseLine(line);
-}).on('close', () => {
-    execute();
-});
+lineReader
+    .on('line', (line) => {
+        parseLine(line);
+    })
+    .on('close', () => {
+        execute();
+    });
 
 export {};
