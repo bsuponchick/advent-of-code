33cb76dc81f08615272f93c635ad4dab69acb985
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2024-12-07 21:46:49 -0600

    Day 7 Problem 2

--- 2024/src/Day7/7.2.ts
+++ 2024/src/Day7/7.2.ts
@@ -1,15 +1,26 @@
-
+import { Equation } from './7.2.logic';
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
+const equations: Equation[] = [];
+const validEquations: Equation[] = [];
 
 const execute = () => {
-    
+    equations.forEach(equation => {
+        if (equation.evaluate()) {
+            validEquations.push(equation);
+        }
+    });
+
+    const sumOfValidEquations = validEquations.reduce((acc, equation) => acc + equation.outcome, 0);
+
+    console.log(`There are ${validEquations.length} valid equations.`);
+    console.log(`The sum of the valid equations is ${sumOfValidEquations}.`);
 }
 
 const parseLine = (line: string) => {
-    
+    equations.push(new Equation(line));
 };
 
 var lineReader = require('readline').createInterface({
