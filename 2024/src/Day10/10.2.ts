56f51c18c4ac446897dfd8701ceb8cad493b49b1
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2024-12-11 09:03:25 -0600

    Day 10 Part 2

--- 2024/src/Day10/10.2.ts
+++ 2024/src/Day10/10.2.ts
@@ -1,14 +1,50 @@
-
+import { Hiker, Tile, HikingMap } from  './10.2.logic';
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
+const hikingMap = new HikingMap();
+const trailheads: Tile[] = [];
+const scores: number[] = [];
+
 const execute = () => {
-    
+    hikingMap.connectTiles();
+
+    if (debug) {
+        console.log(`The hiking map looks like this:\n`);
+        hikingMap.print();
+    }
+
+    trailheads.forEach((trailhead, index) => {
+        const hiker = new Hiker(trailhead, { debug });
+        hiker.hike();
+
+        const score = hikingMap.calculateScore();
+        console.log(`Trailhead ${index} has a score of ${score}`);
+        scores.push(score);
+
+        hikingMap.reset();
+    });
+
+    let sumOfScores = scores.reduce((acc, score) => acc + score, 0);
+    console.log(`The sum of all scores is ${sumOfScores}`);
 }
 
 const parseLine = (line: string) => {
-    
+    const row: Tile[] = [];
+
+    line.split('').forEach((character, index) => {
+        const value = Number.parseInt(character, 10);
+        const tile = new Tile(value);
+
+        row.push(tile);
+
+        if (value === 0) {
+            trailheads.push(tile);
+        }
+    });
+
+    hikingMap.addRow(row);
 };
 
 var lineReader = require('readline').createInterface({
