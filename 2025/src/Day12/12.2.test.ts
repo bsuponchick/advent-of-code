8b6e8597443e5e25cb5e2a92efe2b853e4d9bbb2
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2025-11-21 12:09:01 -0600

    Setup for 2025

---
+++ 2025/src/Day12/12.2.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from '@jest/globals';
+import { subtract } from './12.2.logic';
+
+describe('Day  - Part 2', () => {
+    describe(`When the subtract function is called...`, () => {
+        test(`with 1 and 2, it should return -1`, () => {
+            expect(subtract(1, 2)).toBe(-1);
+        });
+
+        test(`with 2 and 3, it should return -1`, () => {
+            expect(subtract(2, 3)).toBe(-1);
+        });
+
+        test(`with 3 and 4, it should return -1`, () => {
+            expect(subtract(3, 4)).toBe(-1);
+        });
+    });
+});
