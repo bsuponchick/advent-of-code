031d2eb30d63d5704355b79ac91b888d4eabb26d
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2025-12-04 10:20:20 -0800

    2016 Day 1 Problem 1

--- 2016/src/Day1/1.1.logic.ts
+++ 2016/src/Day1/1.1.logic.ts
@@ -1,3 +1,107 @@
-export const add = (a: number, b: number): number => {
-    return a + b;
+import { Coordinate } from '../utils/interfaces/coordinate';
+
+export enum Direction {
+    North = 'north',
+    East = 'east',
+    South = 'south',
+    West = 'west',
+}
+
+interface Instruction {
+    turn: string;
+    steps: number;
+}
+
+export class Traveler {
+    coordinate: Coordinate;
+    direction: Direction;
+
+    constructor(coordinate: Coordinate) {
+        this.coordinate = coordinate;
+        this.direction = Direction.North;
+    }
+
+    faceDirection(direction: Direction): void {
+        this.direction = direction;
+    }
+
+    turnRight(): void {
+        switch (this.direction) {
+            case Direction.North:
+                this.direction = Direction.East;
+                break;
+            case Direction.East:
+                this.direction = Direction.South;
+                break;
+            case Direction.South:
+                this.direction = Direction.West;
+                break;
+            case Direction.West:
+                this.direction = Direction.North;
+                break;
+            default:
+                break;
+        }
+    }
+
+    turnLeft(): void {
+        switch (this.direction) {
+            case Direction.North:
+                this.direction = Direction.West;
+                break;
+            case Direction.West:
+                this.direction = Direction.South;
+                break;
+            case Direction.South:
+                this.direction = Direction.East;
+                break;
+            case Direction.East:
+                this.direction = Direction.North;
+                break;
+            default:
+                break;
+        }
+    }
+
+    moveForward(steps: number): void {
+        switch (this.direction) {
+            case Direction.North:
+                this.coordinate.y += steps;
+                break;
+            case Direction.East:
+                this.coordinate.x += steps;
+                break;
+            case Direction.South:
+                this.coordinate.y -= steps;
+                break;
+            case Direction.West:
+                this.coordinate.x -= steps;
+                break;
+            default:
+                break;
+        }
+    }
+
+    parseInstruction(instruction: string): Instruction {
+        let turn = instruction.charAt(0);
+        let steps = parseInt(instruction.substring(1));
+
+        return { turn, steps};
+    }
+
+    followInstructions(instructions: string[]): void {
+        instructions.forEach(instruction => {
+            const { turn, steps } = this.parseInstruction(instruction);
+            if (turn === 'R') {
+                this.turnRight();
+            } else {
+                this.turnLeft();
+            }
+            this.moveForward(steps);
+        });
+    }
+
+    calculateDistance(): number {
+        return Math.abs(this.coordinate.x) + Math.abs(this.coordinate.y);
+    }
 }
--- 2016/src/Day1/1.1.ts
+++ 2016/src/Day1/1.1.ts
@@ -1,18 +1,20 @@
-import { add } from './1.1.logic';
+import { Traveler, Direction } from './1.1.logic';
 
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-let message: string = '';
+let instructions: string[] = [];
 
 const execute = () => {
-    console.log(`The message is ${message}`);
-    console.log(`Get ready for AoC 2016!`);
+    const traveler = new Traveler({ x: 0, y: 0 });
+    traveler.faceDirection(Direction.North);
+    traveler.followInstructions(instructions);
+    console.log(`The distance is ${traveler.calculateDistance()}`);
 }
 
 const parseLine = (line: string) => {
-   message = line;
+   instructions.push(...line.split(', '));
 };
 
 var lineReader = require('readline').createInterface({
