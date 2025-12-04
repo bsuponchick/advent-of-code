61045bfa8e633e0455dc47dea819fa9a4d3f4aa4
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2025-12-04 13:33:47 -0800

    2016 Day 2 Problem 2

--- 2016/src/Day2/2.2.logic.ts
+++ 2016/src/Day2/2.2.logic.ts
@@ -1,3 +1,135 @@
-export const add = (a: number, b: number): number => {
-    return a + b;
+export class Button {
+    value: string;
+    up: Button | null;
+    down: Button | null;
+    left: Button | null;
+    right: Button | null;
+
+    constructor(value: string) {
+        this.value = value;
+        this.up = null;
+        this.down = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    setUp(button: Button) {
+        this.up = button;
+    }
+
+    setDown(button: Button) {
+        this.down = button;
+    }
+
+    setLeft(button: Button) {
+        this.left = button;
+    }
+
+    setRight(button: Button) {
+        this.right = button;
+    }
+
+    getValue(): string {
+        return this.value;
+    }
+}
+
+export class Keypad {
+    buttons: Button[][];
+    currentButton: Button;
+
+    constructor() {
+        this.buttons = [];
+
+        // Keypad layout is now:
+        //    1
+        //  2 3 4
+        //5 6 7 8 9
+        //  A B C
+        //    D
+
+        this.buttons.push([null, null, new Button('1'), null, null]);
+        this.buttons.push([null, new Button('2'), new Button('3'), new Button('4'), null]);
+        this.buttons.push([new Button('5'), new Button('6'), new Button('7'), new Button('8'), new Button('9')]);
+        this.buttons.push([null, new Button('A'), new Button('B'), new Button('C'), null]);
+        this.buttons.push([null, null, new Button('D'), null, null]);
+
+        // Create the connections between the buttons
+        this.establishConnections();
+
+        // Start with the 5 key
+        this.setCurrentButton(this.buttons[2][0]);
+    }
+
+    establishConnections() {
+        this.buttons.forEach((row, y) => {
+            row.forEach((tile, x) => {
+                if (tile === null) {
+                    return;
+                }
+
+                if (y > 0) {
+                    tile.up = this.buttons[y - 1][x];
+                }
+
+                if (y < this.buttons.length - 1) {
+                    tile.down = this.buttons[y + 1][x];
+                }
+
+                if (x > 0) {
+                    tile.left = this.buttons[y][x - 1];
+                }
+
+                if (x < row.length - 1) {
+                    tile.right = this.buttons[y][x + 1];
+                }
+            });
+        });
+    }
+
+    setCurrentButton(button: Button) {
+        this.currentButton = button;
+    }
+
+    getCurrentButton(): Button {
+        return this.currentButton;
+    }
+
+    move(direction: string) {
+        switch (direction) {
+            case 'U':
+                if (this.currentButton.up === null) {
+                    return;
+                }
+                this.currentButton = this.currentButton.up;
+                break;
+            case 'D':
+                if (this.currentButton.down === null) {
+                    return;
+                }
+                this.currentButton = this.currentButton.down;
+                break;
+            case 'L':
+                if (this.currentButton.left === null) {
+                    return;
+                }
+                this.currentButton = this.currentButton.left;
+                break;
+            case 'R':
+                if (this.currentButton.right === null) {
+                    return;
+                }
+                this.currentButton = this.currentButton.right;
+                break;
+            default:
+                throw new Error('Invalid direction');
+        }
+    }
+
+    followDirectionsAndReturnCode(directions: string): string {
+        directions.split('').forEach((direction) => {
+            this.move(direction);
+        });
+        return this.currentButton.getValue();
+    }
 }
--- 2016/src/Day2/2.2.test.ts
+++ 2016/src/Day2/2.2.test.ts
@@ -1,18 +1,156 @@
 import { describe, expect, test } from '@jest/globals';
-import { add } from './2.2.logic';
+import { Button, Keypad } from './2.2.logic';
 
 describe('Day 2 - Part 2', () => {
-    describe(`When the add function is called...`, () => {
-        test(`with 1 and 2, it should return 3`, () => {
-            expect(add(1, 2)).toBe(3);
+    describe(`Button...`, () => {
+        describe(`constructor`, () => {
+            test(`should create a button with the correct value`, () => {
+                const button = new Button('1');
+                expect(button.value).toBe('1');
+                expect(button.up).toBeNull();
+                expect(button.down).toBeNull();
+                expect(button.left).toBeNull();
+                expect(button.right).toBeNull();
+            });
         });
 
-        test(`with 2 and 3, it should return 5`, () => {
-            expect(add(2, 3)).toBe(5);
+        describe(`setUp`, () => {
+            test(`should set the up button`, () => {
+                const button = new Button('1');
+                const upButton = new Button('2');
+                button.setUp(upButton);
+                expect(button.up).toBe(upButton);
+            });
+        });
+        
+        describe(`setDown`, () => {
+            test(`should set the down button`, () => {
+                const button = new Button('1');
+                const downButton = new Button('2');
+                button.setDown(downButton);
+                expect(button.down).toBe(downButton);
+            });
+        });
+        
+        describe(`setLeft`, () => {
+            test(`should set the left button`, () => {
+                const button = new Button('1');
+                const leftButton = new Button('2');
+                button.setLeft(leftButton);
+                expect(button.left).toBe(leftButton);
+            });
+        });
+        
+        describe(`setRight`, () => {
+            test(`should set the right button`, () => {
+                const button = new Button('1');
+                const rightButton = new Button('2');
+                button.setRight(rightButton);
+                expect(button.right).toBe(rightButton);
+            });
+        });
+        
+        describe(`getValue`, () => {
+            test(`should return the value of the button`, () => {
+                const button = new Button('1');
+                expect(button.getValue()).toBe('1');
+            });
+        });
+    });
+
+    describe(`Keypad...`, () => {
+        describe(`constructor`, () => {
+            test(`should create a keypad with the correct buttons`, () => {
+                const keypad = new Keypad();
+                expect(keypad.buttons.length).toBe(5);
+                expect(keypad.currentButton.getValue()).toBe('5');
+            });
         });
 
-        test(`with 3 and 4, it should return 7`, () => {
-            expect(add(3, 4)).toBe(7);
+        describe(`When the setCurrentButton method is called...`, () => {
+            test(`should set the current button`, () => {
+                const keypad = new Keypad();
+                keypad.setCurrentButton(keypad.buttons[0][0]);
+                expect(keypad.currentButton).toBe(keypad.buttons[0][0]);
+            });
+        });
+
+        describe(`When the getCurrentButton method is called...`, () => {
+            test(`should return the current button`, () => {
+                const keypad = new Keypad();
+                expect(keypad.getCurrentButton().value).toBe('5');
+            });
+        });
+
+        describe(`When the move method is called...`, () => {
+            test(`should move the current button up if the direction is U`, () => {
+                const keypad = new Keypad();
+                keypad.setCurrentButton(keypad.buttons[2][2]);
+                keypad.move('U');
+                expect(keypad.currentButton).toBe(keypad.buttons[1][2]);
+            });
+
+            test(`should move the current button down if the direction is D`, () => {
+                const keypad = new Keypad();
+                keypad.setCurrentButton(keypad.buttons[2][2]);
+                keypad.move('D');
+                expect(keypad.currentButton).toBe(keypad.buttons[3][2]);
+            });
+
+            test(`should move the current button left if the direction is L`, () => {
+                const keypad = new Keypad();
+                keypad.setCurrentButton(keypad.buttons[2][2]);
+                keypad.move('L');
+                expect(keypad.currentButton).toBe(keypad.buttons[2][1]);
+            });
+
+            test(`should move the current button right if the direction is R`, () => {
+                const keypad = new Keypad();
+                keypad.setCurrentButton(keypad.buttons[2][2]);
+                keypad.move('R');
+                expect(keypad.currentButton).toBe(keypad.buttons[2][3]);
+            });
+
+            test(`should not move the current button if the direction is invalid`, () => {
+                const keypad = new Keypad();
+                expect(() => keypad.move('X')).toThrow('Invalid direction');
+            });
+
+            test(`should not move the current button up if the direction is out of bounds`, () => {
+                const keypad = new Keypad();
+                keypad.setCurrentButton(keypad.buttons[0][2]);
+                keypad.move('U');
+                expect(keypad.currentButton).toBe(keypad.buttons[0][2]);
+            });
+
+            test(`should not move the current button down if the direction is out of bounds`, () => {
+                const keypad = new Keypad();
+                keypad.setCurrentButton(keypad.buttons[4][2]);
+                keypad.move('D');
+                expect(keypad.currentButton).toBe(keypad.buttons[4][2]);
+            });
+            
+            test(`should not move the current button left if the direction is out of bounds`, () => {
+                const keypad = new Keypad();
+                keypad.setCurrentButton(keypad.buttons[2][0]);
+                keypad.move('L');
+                expect(keypad.currentButton).toBe(keypad.buttons[2][0]);
+            });
+            
+            test(`should not move the current button right if the direction is out of bounds`, () => {
+                const keypad = new Keypad();
+                keypad.setCurrentButton(keypad.buttons[2][4]);
+                keypad.move('R');
+                expect(keypad.currentButton).toBe(keypad.buttons[2][4]);
+            });
+        });
+
+        describe(`When the followDirectionsAndReturnCode method is called...`, () => {
+            test(`should return the code for the directions`, () => {
+                const keypad = new Keypad();
+                const code = keypad.followDirectionsAndReturnCode('UDLR');
+                expect(code).toBe('6');
+            });
         });
     });
 });
