529652ca8aea66d9b585d3e4f923f3f8c9854890
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2025-12-05 13:51:51 -0800

    2016 Day 10 Problem 2

--- 2016/src/Day10/10.2.ts
+++ 2016/src/Day10/10.2.ts
@@ -1,18 +1,76 @@
-import { add } from './10.2.logic';
+import { Bot, Bin, Microchip } from './10.1.logic';
 
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-let message: string = '';
+let chipInstructions: string[] = [];
+
+let bots: Map<number, Bot> = new Map();
+let bins: Map<number, Bin> = new Map();
+const lowGoalId = test ? 2 : 17;
+const highGoalId = test ? 5 : 61;
 
 const execute = () => {
-    console.log(`The message is ${message}`);
-    console.log(`Get ready for AoC 2016!`);
+    console.log(`Bots: ${bots.size}`);
+    console.log(`Bins: ${bins.size}`);
+
+    for (const instruction of chipInstructions) {
+        const parts = instruction.split(' ');
+        const value = Number(parts[1]);
+        const botId = Number(parts[5]);
+        bots.get(botId).addMicrochip(new Microchip(value));
+    }
+
+    let product = 1;
+    for (const bin of bins.values()) {
+        if (bin.id === 0 || bin.id === 1 || bin.id === 2) {
+            product *= bin.microchips[0].value;
+        }
+    }
+    console.log(`Product: ${product}`);
 }
 
 const parseLine = (line: string) => {
-   message = line;
+    if (line.startsWith('value')) {
+        chipInstructions.push(line);
+    } else {
+        // This is a bot instruction, need to set up all bots and bins we can
+        const parts = line.split(' ');
+        const botId = Number(parts[1]);
+        const lowTargetId = Number(parts[6]);
+        const lowTargetType = parts[5];
+        const highTargetId = Number(parts[11]);
+        const highTargetType = parts[10];
+
+        if (!bots.has(botId)) {
+            bots.set(botId, new Bot(botId, lowGoalId, highGoalId));
+        }
+
+        if (lowTargetType === 'bot') {
+            if (!bots.has(lowTargetId)) {
+                bots.set(lowTargetId, new Bot(lowTargetId, lowGoalId, highGoalId));
+            }
+        } else {
+            if (!bins.has(lowTargetId)) {
+                bins.set(lowTargetId, new Bin(lowTargetId));
+            }
+        }
+
+        if (highTargetType === 'bot') {
+            if (!bots.has(highTargetId)) {
+                bots.set(highTargetId, new Bot(highTargetId, lowGoalId, highGoalId));
+            }
+        } else {
+            if (!bins.has(highTargetId)) {
+                bins.set(highTargetId, new Bin(highTargetId));
+            }
+        }
+        
+        // Now we set the targets for the bot
+        bots.get(botId).setLowTarget(lowTargetType === 'bot' ? bots.get(lowTargetId) : bins.get(lowTargetId));
+        bots.get(botId).setHighTarget(highTargetType === 'bot' ? bots.get(highTargetId) : bins.get(highTargetId));
+    }
 };
 
 var lineReader = require('readline').createInterface({
