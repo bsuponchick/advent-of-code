08b28c26cd3beeaf075db125fb1bbce614a54241
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2025-12-05 09:27:23 -0800

    2016 Day 8 Part 1

--- 2016/src/Day8/8.1.logic.ts
+++ 2016/src/Day8/8.1.logic.ts
@@ -1,3 +1,86 @@
-export const add = (a: number, b: number): number => {
-    return a + b;
+export class Screen {
+    pixels: boolean[][] = [];
+
+    constructor(width: number, height: number) {
+        this.pixels = [];
+
+        for (let i = 0; i < height; i++) {
+            this.pixels[i] = [];
+
+            for (let j = 0; j < width; j++) {
+                this.pixels[i][j] = false;
+            }
+        }
+    }
+
+    rect(width: number, height: number) {
+        for (let i = 0; i < height; i++) {
+            for (let j = 0; j < width; j++) {
+                this.pixels[i][j] = true;
+            }
+        }
+    }
+
+    rotateRow(row: number, amount: number) {
+        const rowPixels = this.pixels[row];
+
+        for (let i = 0; i < amount; i++) {
+            rowPixels.unshift(rowPixels.pop());
+        }
+    }
+
+    rotateColumn(column: number, amount: number) {
+        const columnPixels = this.pixels.map((row) => row[column]);
+
+        for (let i = 0; i < amount; i++) {
+            columnPixels.unshift(columnPixels.pop());
+        }
+        
+        for (let i = 0; i < this.pixels.length; i++) {
+            this.pixels[i][column] = columnPixels[i];
+        }
+    }
+
+    followCommand(command: string) {
+        if (command.startsWith('rect')) {
+            // Issue the rect command, it will be in the format of "rect <width>x<height>"
+            const parts = command.split(' ');
+            const [width, height] = parts[1].split('x');
+            this.rect(Number(width), Number(height));
+        } else if (command.startsWith('rotate row y=')) {
+            // Issue the rotate row command, it will be in the format of "rotate row y=<row> by <amount>"
+            const parts = command.split('rotate row y=');
+            const [row, amount] = parts[1].split(' by ');
+            this.rotateRow(Number(row), Number(amount));
+        } else if (command.startsWith('rotate column x=')) {
+            // Issue the rotate column command, it will be in the format of "rotate column x=<column> by <amount>"
+            const parts = command.split('rotate column x=');
+            const [column, amount] = parts[1].split(' by ');
+            this.rotateColumn(Number(column), Number(amount));
+        }
+        else {
+            throw new Error(`Invalid command: ${command}`);
+        }
+    }
+
+    countPixelsOn(): number {
+        return this.pixels.reduce((sum, row) => {
+            return sum + row.reduce((sum, pixel) => {
+                return sum + (pixel ? 1 : 0);
+            }, 0);
+        }, 0);
+    }
+
+    print() {
+        let output = '';
+
+        for (let i = 0; i < this.pixels.length; i++) {
+            for (let j = 0; j < this.pixels[i].length; j++) {
+                output += this.pixels[i][j] ? '#' : '.';
+            }
+            output += '\n';
+        }
+
+        console.log(output);
+    }
 }
--- 2016/src/Day8/8.1.test.ts
+++ 2016/src/Day8/8.1.test.ts
@@ -1,18 +1,67 @@
 import { describe, expect, test } from '@jest/globals';
-import { add } from './8.1.logic';
+import { Screen } from './8.1.logic';
 
 describe('Day 8 - Part 1', () => {
-    describe(`When the add function is called...`, () => {
-        test(`with 1 and 2, it should return 3`, () => {
-            expect(add(1, 2)).toBe(3);
+    describe(`Screen...`, () => {
+        describe(`Constructor...`, () => {
+            test(`should create a new Screen`, () => {
+                const screen = new Screen(3, 2);
+                expect(screen.pixels).toEqual([[false, false, false], [false, false, false]]);
+            });
         });
 
-        test(`with 2 and 3, it should return 5`, () => {
-            expect(add(2, 3)).toBe(5);
+        describe(`When rect is called...`, () => {
+            test(`should set the pixels to true for teh given width and height`, () => {
+                const screen = new Screen(3, 2);
+                screen.rect(2, 1);
+                expect(screen.pixels).toEqual([[true, true, false], [false, false, false]]);
+            });
+
+            test(`should set the pixels to true for the given width and height`, () => {
+                const screen = new Screen(7, 3);
+                screen.rect(3, 2);
+                expect(screen.pixels).toEqual([[true, true, true, false, false, false, false], [true, true, true, false, false, false, false], [false, false, false, false, false, false, false]]);
+            });
         });
 
-        test(`with 3 and 4, it should return 7`, () => {
-            expect(add(3, 4)).toBe(7);
+        describe(`When rotateRow is called...`, () => {
+            test(`should rotate the row to the right by the given amount`, () => {
+                const screen = new Screen(5, 3);
+                screen.rect(3, 2);
+                screen.rotateRow(0, 1);
+                expect(screen.pixels).toEqual([[false, true, true, true, false], [true, true, true, false, false], [false, false, false, false, false]]);
+            });
+
+            test(`should handle overflow correctly`, () => {
+                const screen = new Screen(5, 3);
+                screen.rect(3, 2);
+                screen.rotateRow(0, 3);
+                expect(screen.pixels).toEqual([[true, false, false, true, true], [true, true, true, false, false], [false, false, false, false, false]]);
+            });
+        });
+
+        describe(`When rotateColumn is called...`, () => {
+            test(`should rotate the column to the right by the given amount`, () => {
+                const screen = new Screen(5, 3);
+                screen.rect(3, 2);
+                screen.rotateColumn(0, 1);
+                expect(screen.pixels).toEqual([[false, true, true, false, false], [true, true, true, false, false], [true, false, false, false, false]]);
+            });
+
+            test(`should handle overflow correctly`, () => {
+                const screen = new Screen(5, 3);
+                screen.rect(3, 2);
+                screen.rotateColumn(0, 2);
+                expect(screen.pixels).toEqual([[true, true, true, false, false], [false, true, true, false, false], [true, false, false, false, false]]);
+            });
+        });
+
+        describe(`When countPixelsOn is called...`, () => {
+            test(`should return the number of pixels on the screen`, () => {
+                const screen = new Screen(5, 3);
+                screen.rect(3, 2);
+                expect(screen.countPixelsOn()).toBe(6);
+            });
         });
     });
 });
