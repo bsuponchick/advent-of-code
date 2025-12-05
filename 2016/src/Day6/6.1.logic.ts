e9dbd14e6769e5111c7fda02ac1f7291a22613d8
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2025-12-04 16:37:53 -0800

    2016 Day 6 Part 1

--- 2016/src/Day6/6.1.logic.ts
+++ 2016/src/Day6/6.1.logic.ts
@@ -1,3 +1,34 @@
-export const add = (a: number, b: number): number => {
-    return a + b;
+export class SignalDecoder {
+    signals: string[];
+    columnCounts: { [key: string]: number }[];
+
+    constructor(signals: string[]) {
+        this.signals = signals;
+        this.columnCounts = [];
+
+        this.parseSignals();
+    }
+
+    parseSignals(): void {
+        this.signals.forEach((signal) => {
+            signal.split('').forEach((char, index) => {
+                if (!this.columnCounts[index]) {
+                    this.columnCounts[index] = {};
+                }
+
+                this.columnCounts[index][char] = (this.columnCounts[index][char] || 0) + 1;
+            });
+        });
+    }
+
+    decode(): string {
+        let decodedMessage = '';
+
+        this.columnCounts.forEach((columnCount) => {
+            const mostCommonChar = Object.keys(columnCount).reduce((a, b) => columnCount[a] > columnCount[b] ? a : b);
+            decodedMessage += mostCommonChar;
+        });
+
+        return decodedMessage;
+    }
 }
