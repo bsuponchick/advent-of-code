86719e2005cd7c2340dccfe8fdae832a0bad0288
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2025-12-10 19:16:57 -0600

    Fixed test

--- 2025/src/Day10/10.1.test.ts
+++ 2025/src/Day10/10.1.test.ts
@@ -5,10 +5,11 @@ describe('Day 10 - Part 1', () => {
     describe(`Machine`, () => {
         describe(`Constructor`, () => {
             test(`should initialize the machine correctly`, () => {
-                const machine = new Machine('.##', [new Button([1, 2, 3]), new Button([4]), new Button([5, 6])]);
-                expect(machine.startingState).toEqual('...');
+                const machine = new Machine('.##', [new Button([1, 2, 3]), new Button([4]), new Button([5, 6])], [7, 8, 9]);
+                expect(machine.currentState).toEqual('...');
                 expect(machine.goalState).toEqual('.##');
                 expect(machine.buttons.length).toBe(3);
+                expect(machine.joltage.length).toBe(3);
             });
         });
     });
