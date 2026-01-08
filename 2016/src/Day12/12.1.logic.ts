c7cc936577dca3d3abaf58a793ecf5f3b900b55a
Brandon Suponchick <brandon.suponchick@aviture.us.com>
2026-01-08 12:04:14 -0600

    2016 Day 12

--- 2016/src/Day12/12.1.logic.ts
+++ 2016/src/Day12/12.1.logic.ts
@@ -1,3 +1,71 @@
-export const add = (a: number, b: number): number => {
-    return a + b;
+export const cpy = (x: string, y: string, registers: { [key: string]: number }) => {
+    let value = 0;
+
+    if (registers[x] === undefined) {
+        value = Number(x);
+    } else {
+        value = registers[x];
+    }
+
+    console.log(`Setting register ${y} to ${x}`);
+    registers[y] = value;
+}
+
+export const inc = (x: string, registers: { [key: string]: number }) => {
+    console.log(`Current value of register ${x} is ${registers[x]}`);
+    console.log(`Incrementing register ${x} to ${registers[x] + 1}`);
+    const value = registers[x] || 0;
+    registers[x] = value + 1;
+}
+
+export const dec = (x: string, registers: { [key: string]: number }) => {
+    const value = registers[x] || 0;
+    registers[x] = value - 1;
+}
+
+export const jnz = (x: string, y: string, registers: { [key: string]: number }, instructionPointer: number): number => {
+    let value = 0;
+    if (registers[x] === undefined) {
+        value = Number(x);
+    } else {
+        value = registers[x];
+    }
+
+    if (value !== 0) {
+        return instructionPointer + Number(y);
+    }
+
+    return instructionPointer + 1;
+}
+
+export class Instruction { 
+    public type: string;
+    public x: string;
+    public y: string;
+
+    constructor(type: string, x: string, y: string| null) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+    }
+
+    execute(instructionPointer: number, registers: { [key: string]: number }): number {
+        switch (this.type) {
+            case 'cpy':
+                cpy(this.x, this.y as string, registers);
+                break;
+            case 'inc':
+                inc(this.x, registers);
+                break;
+            case 'dec':
+                dec(this.x, registers);
+                break;
+            case 'jnz':
+                return jnz(this.x, this.y, registers, instructionPointer);
+            default:
+                throw new Error(`Invalid instruction: ${this.type}`);
+        }
+
+        return instructionPointer + 1;
+    }
 }
--- 2016/src/Day12/12.2.ts
+++ 2016/src/Day12/12.2.ts
@@ -1,18 +1,32 @@
-import { add } from './12.2.logic';
+import { Instruction } from './12.1.logic';
 
 const args = process.argv;
 const debug = args.includes('--debug');
 const test = args.includes('--test');
 
-let message: string = '';
+let instructions: Instruction[] = [];
+let registers: { [key: string]: number } = {};
 
 const execute = () => {
-    console.log(`The message is ${message}`);
-    console.log(`Get ready for AoC 2016!`);
+    console.log(`There are ${instructions.length} instructions`);
+    console.log(`Instructions: ${JSON.stringify(instructions)}`);
+
+    let instructionPointer = 0;
+    registers = { a: 0, b: 0, c: 1, d: 0 };
+
+    while (instructionPointer < instructions.length) {
+        const instruction = instructions[instructionPointer];
+        instructionPointer = instruction.execute(instructionPointer, registers);
+
+        console.log(`Registers is ${JSON.stringify(registers)}`);
+    }
+
+    console.log(`The value of register a is ${registers['a']}`);
 }
 
 const parseLine = (line: string) => {
-   message = line;
+    const parts = line.split(' ');
+    instructions.push(new Instruction(parts[0], parts[1], parts[2] ? parts[2] : null));
 };
 
 var lineReader = require('readline').createInterface({
